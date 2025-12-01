0a8db39a337da1eaf155a88f4831d30e7679e136
Joseph <josephfabrello@gmail.com>
2025-12-01 17:26:54 +1100

    stubbed out network tests

---
+++ tests/test_monitor_network.py
@@ -0,0 +1,35 @@
+import pytest
+from multiprocessing import Queue, Process
+
+
+
+
+class TestNetworkMonitor:
+    """Tests for the monitor_network.py file in src/network/monitor_network.py"""
+
+    def test_intialise_NetworkMonitor_class(self):
+        pass
+
+    def test_start_returns_process(self):
+        pass
+
+    def test_start_returns_None(self):
+        pass
+
+    def test_stop_return_true(self):
+        pass
+
+    def test_stop_return_false(self):
+        pass
+
+    def test_cycle_adds_to_queue(self):
+        pass
+
+    def test_cycle_handles_bad_packet(self):
+        pass
+
+    def test_deconstruct_packet_correctly(self):
+        pass
+
+    def test_deconstruct_malformed_packet(self):
+        pass
